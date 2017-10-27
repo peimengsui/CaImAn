89ff47027e85c21d9380de6cf36c125e2b19d129
Peimeng Sui <speimeng@worker0031.cm.cluster>
2017-10-27 16:47:42 -0400

    delete files

--- use_cases/edge-cutter/neuron_regressor/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -N3 --exclusive --ntasks-per-node=5
-# Start from an "empty" module collection.
-module purge
-# Load in what we need to execute mpirun.
-module load slurm gcc openmpi
-mpirun ./mpi_hello_world
