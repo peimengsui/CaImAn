02f9d73aef03e3a5d5434a96a28b36866e0c3b1c
Peimeng Sui <speimeng@worker0031.cm.cluster>
2017-11-03 16:53:17 -0400

    add error analysis code

--- use_cases/edge-cutter/neuron_regressor/predict.py
+++ use_cases/edge-cutter/neuron_regressor/predict.py
@@ -1,10 +1,14 @@
 import torch
 import pickle
+import vgg
+import numpy as np
+from data_loader import *
+import pandas as pd
 
-regressor = torch.load('save_temp/checkpoint_63.tar')
-import pdb
-pdb.set_trace()
-
+model_dict = torch.load('save_temp/checkpoint_63.tar')['state_dict']
+regressor = vgg.__dict__['vgg11_bn']()
+regressor.load_state_dict(model_dict)
+regressor = regressor.cpu()
 no_neuron_image = np.load('/mnt/ceph/neuro/edge_cutter/25_zero_input_data/Yr_d1_512_d2_512_d3_1_order_C_frames_8000_.test.npz')
 no_neuron_image = no_neuron_image['arr_0']
 
@@ -14,7 +18,7 @@ test_image  = np.load('/mnt/ceph/neuro/edge_cutter/25_input_data/Yr_d1_512_d2_51
 test_image = test_image['arr_0']
 test_locations = pickle.load(open('/mnt/ceph/neuro/edge_cutter/25_input_data/Yr_d1_512_d2_512_d3_1_order_C_frames_8000_.test.pkl', 'rb')) 
 
-test_loader = data_generator(test_image, test_locations, no_neuron_image, no_neuron_locations, batch_size=1, train=False)
+test_loader = data_generator(test_image, test_locations, no_neuron_image, no_neuron_locations, batch_size=128, train=False)
 
 def predict(test_loader, model):
     """
@@ -28,24 +32,25 @@ def predict(test_loader, model):
 
     n_val = 0
     #for i, (input, target) in enumerate(val_loader):
-    for i, (input, target) in val_loader:
-        data_list.append(input.numpy())
-        ground_truth.append(int(target.numpy()))
+    for i, (input, target) in test_loader:
+        data_list.append(input.numpy().reshape((128, 64, 64)))
+        ground_truth += [int(x) for x in target.numpy().flatten()]
         input_var = torch.autograd.Variable(input, volatile=True)
-        if torch.cuda.is_available():
-            input_var = input_var.cuda()
+        #if torch.cuda.is_available():
+        #    input_var = input_var.cuda()
 
         # compute output
-        prediction.append(float(model(input_var).cpu().data.numpy()))
+        prediction += [float(x) for x in model(input_var).cpu().data.numpy().flatten()]
         
         n_val += 1
-        if n_val > 1024:
+        if n_val > 60:
             break
 
 
     return data_list, prediction, ground_truth
 
 data_list, prediction, ground_truth = predict(test_loader, regressor)
-pickle.dump(data_list, open('save_temp/prediction_images.pkl'))
-
+pickle.dump(data_list, open('save_temp/prediction_images.pkl',"wb"))
+df = pd.DataFrame({'prediction':prediction,'ground_truth':ground_truth})
+df.to_csv('save_temp/prediction.csv')
 
---
+++ use_cases/edge-cutter/neuron_regressor/predict_batch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=myPythonJobGPU
+#SBATCH --nodes=1
+#SBATCH --time=20:00:00
+#SBATCH --mem=50GB
+#SBATCH --gres=gpu:2
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ps3336@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+#module purge
+#module load gcc/7.2.0
+#module load python3/3.6.2
+#module load cuda/8.0.61
+#pip3 install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp36-cp36m-manylinux1_x86_64.whl 
+#pip3 install torchvision
+
+RUNDIR=/mnt/home/speimeng/dev/CaImAn/use_cases/edge-cutter/neuron_regressor
+
+DATADIR=$SCRATCH/my_project/data
+
+cd $RUNDIR
+
+python predict.py
+
